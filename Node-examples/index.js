c9236b7ea0b7f96580762e721c823639100aee1b
go367it <satyamkumar3001@gmail.com>
2020-12-07 17:28:35 +0530

    Simple Node Examples

---
+++ Node-examples/index.js
@@ -0,0 +1,21 @@
+let rect = {
+    perimeter: (x,y) => (2*(x+y)),
+    area: (x,y) => (x*y)
+};
+
+function solveRect(l,b) {
+    console.log("solving for rectangle width and length")
+
+    if(l <= 0 || b <= 0 ){
+        console.log("length and breadth must be greater than 0")
+    }
+    else {
+        console.log("perimeter" + rect.perimeter(l,b));
+        console.log("area:" + rect.area(l,b));
+    }
+}
+
+solveRect(4,2);
+solveRect(0,1);
+solveRect(2,2);
+solveRect(5,-3);
