244443d21554f9fdc875fe2e5dec9f2170fb0f1c
go367it <satyamkumar3001@gmail.com>
2021-03-03 21:55:26 +0530

    node callbacks and error handling

--- Node-examples/Rectangle.js
+++ Node-examples/Rectangle.js
@@ -1,2 +1,18 @@
-exports.perimeter = (x,y) => (2*(x+y));
-exports.area = (x,y) => (x*y);
+module.exports = (x, y, callback) => {
+    if(x <= 0 || y <= 0){
+        setTimeout(()=>{
+            callback(new Error('something went wrong'),null)
+        }, 3000)
+    }
+    else{
+        setTimeout(()=>{
+            callback(null,
+                {
+                    perimeter : (x,y) => (2*(x+y)),
+                    area : (x,y) => (x*y)
+                }
+                )
+        }, 3000)
+    }
+}
+
--- Node-examples/index.js
+++ Node-examples/index.js
@@ -3,13 +3,19 @@ let rect = require('./Rectangle');
 function solveRect(l,b) {
     console.log("solving for rectangle width and length")
 
-    if(l <= 0 || b <= 0 ){
-        console.log("length and breadth must be greater than 0")
-    }
-    else {
-        console.log("perimeter" + rect.perimeter(l,b));
-        console.log("area:" + rect.area(l,b));
-    }
+    rect(l, b, (err, rectangle) => {
+
+        if(err){
+            console.log("Error:",err.message)
+        }
+        else{
+
+            console.log(`area of the rectangle ${rectangle.area()}`)
+
+        }
+
+    })
+
 }
 
 solveRect(4,2);
