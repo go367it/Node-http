59ab2c8745928d8e60e2f74f5921c7e4de93adbb
go367it <satyamkumar3001@gmail.com>
2021-05-17 00:31:35 +0530

    express router

--- node-express/index.js
+++ node-express/index.js
@@ -3,6 +3,7 @@ const http = require('http')
 const morgan = require('morgan')
 const bodyParser = require('body-parser')
 
+const dishRouter = require('./routes/dishRouter') // importing dishrouter
 const hostname = 'localhost'
 const port = 3000
 
@@ -10,62 +11,8 @@ const app = express()
 app.use(morgan('dev'))
 app.use(bodyParser.json())
 
-app.all('/dishes', (req, res, next) =>{
-    res.statusCode = 200
-    res.setHeader('Content-Type', 'text/plain')
-    next()
-})
 
-app.get('/dishes', (req, res, next) =>{
-    res.end('will send all the dishes to you')
-})
-
-app.post('/dishes', (req, res, next) =>{
-    res.end('will add the dish: ' + req.body.name + 
-    ' with details: ' + req.body.description
-    )
-})
-
-app.put('/dishes', (req, res, next) =>{
-    res.statusCode = 403
-    res.end('PUT operation not supported on /dishes')
-})
-
-app.delete('/dishes', (req, res, next) =>{
-    res.end('Deleting all the dishes!')
-})
-
-app.all('/dishes/:dishId', (req, res, next) =>{
-    res.statusCode = 200
-    res.setHeader('Content-Type', 'text/plain')
-    next()
-})
-
-app.get('/dishes/:dishId', (req, res, next) =>{
-    res.end('will send details of the dish: '
-    + req.params.dishId + ' to you!'
-    )
-})
-
-app.post('/dishes/:dishId', (req, res, next) =>{
-    res.statusCode = 403
-    res.end('POST operation not supported on /dishes/'
-    + req.params.dishId
-    )
-})
-
-app.put('/dishes/:dishId', (req, res, next) =>{
-    res.write('updating the dish: '
-    + req.params.dishId)
-
-    res.end(' will update the dish: ' + req.body.name
-    + ' with details: ' + req.body.description
-    )
-})
-
-app.delete('/dishes/:dishId', (req, res, next) =>{
-    res.end('Deleting dish: ' + req.params.dishId)
-})
+app.use('/dishes', dishRouter) // using the dishrouter module to create a different express router
 
 app.use(express.static(__dirname + '/public'))
 
